9c926af425a2a65699cc60b5cd9991f9231f89c8
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2024-02-14 12:23:07 +0300

    initial commit

---
+++ PokemonSimpleApp/PokemonSimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSimpleAppApp.swift
+//  PokemonSimpleApp
+//
+//  Created by Eldar Garbuzov on 14.02.24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonSimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
