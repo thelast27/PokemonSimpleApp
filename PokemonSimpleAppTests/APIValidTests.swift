9e15a56eaa7b19a1435fc747395633b1a18c2cea
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2024-02-19 16:05:34 +0300

    add test for pokemon list api

--- PokemonSimpleAppTests/APIValidTests.swift
+++ PokemonSimpleAppTests/APIValidTests.swift
@@ -10,11 +10,6 @@ import XCTest
 
 final class PokemonSimpleAppTests: XCTestCase {
     
-    private let networkUtils = NetworkUtils.shared
-    private let constants = Constants()
-    private let limit: Int = 20
-    private let offset: Int = 0
-    private let url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png"
     private let urlSession = URLSession(configuration: .default)
     private let networkMonitor = NetworkMonitor.shared
     
@@ -29,8 +24,41 @@ final class PokemonSimpleAppTests: XCTestCase {
         try super.tearDownWithError()
     }
 //MARK: -
-    func testValidApiCallGetsHTTPStatusCode200() throws  {
-        //check accert code200 for "Get image url" 
+    func testValidApiCallCode200PokemonImage() throws  {
+        //check accert code200 for "Get image url"
+        
+        let url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png"
+        
+        try XCTSkipUnless(
+            networkMonitor.isReachable,
+          "Network connectivity needed for this test.")
+        
+        // given
+        guard let url = URL(string: url) else { return }
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+
+        // when
+        let dataTask = urlSession.dataTask(with: url) { _, response, error in
+          statusCode = (response as? HTTPURLResponse)?.statusCode
+          responseError = error
+          promise.fulfill()
+        }
+        dataTask.resume()
+        wait(for: [promise], timeout: 5)
+
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
+    }
+    
+    func testValidApiCallCode200PokemonList() throws {
+        //check accert code200 for "Get pokemon list url"
+        
+        let limit: Int = 20
+        let offset: Int = 0
+        let url = "https://pokeapi.co/api/v2/pokemon?limit=\(limit)&offset=\(offset)"
         
         try XCTSkipUnless(
             networkMonitor.isReachable,
