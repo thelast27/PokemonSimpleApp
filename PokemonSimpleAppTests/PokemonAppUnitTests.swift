ca03b340b2ed425d75f1c244038838b244983779
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2024-02-20 19:09:32 +0300

    add test for detail pokemon fetch + improve pokemon list tests

--- PokemonSimpleAppTests/PokemonAppUnitTests.swift
+++ PokemonSimpleAppTests/PokemonAppUnitTests.swift
@@ -6,48 +6,71 @@
 //
 
 import XCTest
-import SwiftUI
+import CoreData
 @testable import PokemonSimpleApp
 
 class PokemonAppUnitTests: XCTestCase {
     
-    var cd: CoreDataManager?
-    var getPokemonListUseCase: GetPokemonListUseCase?
+    private let limit = 20
+    private let offset = 0
+    private let id = 12
     
-    @Environment(\.managedObjectContext) var context
+    private var coreDataManager: CoreDataManager?
+    private var context: NSManagedObjectContext?
+    private var container: NSPersistentContainer?
+    private var getPokemonList: GetPokemonListUseCase?
+    private var getPokemonDetail: GetPokemonDetailUseCase?
     
     override func setUpWithError() throws {
         try super.setUpWithError()
-        cd = CoreDataManager()
-        getPokemonListUseCase = GetPokemonListUseCase(pokemonRepository: ExploreRepository.shared)
+        coreDataManager = CoreDataManager()
+        container = NSPersistentContainer(name: "CoreJSON")
+        context = container?.viewContext
+        getPokemonList = GetPokemonListUseCase(pokemonRepository: ExploreRepository.shared)
+        getPokemonDetail = GetPokemonDetailUseCase(repository: DetailRepository.shared)
     }
     
     override func tearDownWithError() throws {
-        cd = nil
-        getPokemonListUseCase = nil
+        coreDataManager = nil
+        getPokemonList = nil
+        getPokemonDetail = nil
+        container = nil
+        context = nil
         try super.tearDownWithError()
     }
     
+    //MARK: - tests
+    
     func testCoreData() throws {
-        let storeCount = cd?.container.viewContext.persistentStoreCoordinator?.persistentStores.count
+        let storeCount = coreDataManager?.container.viewContext.persistentStoreCoordinator?.persistentStores.count
         XCTAssertEqual(storeCount, 1, "check for storing data in Core Data")
     }
     
-    func testGetPokemonList() throws {
-        let limit = 20
-        let offset = 0
-        Task {
-            guard let getPokemonList = try await getPokemonListUseCase?.repository.fetchPokemons(context: context, limit: limit, offset: offset) else { return }
-            XCTAssert(!getPokemonList.isEmpty, "pokemon fetch existence check")
-        }
+    func testGetPokemonList() async throws {
+        guard let context = context,
+              let newPokemonList = try? await getPokemonList?.execute(context: context, limit: limit, offset: offset) else { return }
+        var pokemonList: [PokemonEntity] = []
+        
+        pokemonList += newPokemonList
+        
+        XCTAssertEqual(pokemonList.count, 20, "limit pokemon list request excess")
+        XCTAssertFalse(pokemonList.isEmpty, "pokemon fetch existence check")
     }
     
-    func testPokemonCount() throws {
-        let limit = 20
-        let offset = 0
-        Task {
-            guard let pokemonCount = try await getPokemonListUseCase?.repository.fetchPokemons(context: context, limit: limit, offset: offset) else { return }
-            XCTAssertEqual(pokemonCount.count, limit, "limit pokemon list request excess")
-        }
+    func testGetPokemonDetail() async throws {
+        let name = "butterfree"
+        let height = 11
+        let weight = 320
+        let type = "bug"
+        let getPokemonDetailUseCase = GetPokemonDetailUseCase(repository: DetailRepository.shared)
+        var pokemonDetail: PokemonDetailEntity?
+        
+        pokemonDetail = try await getPokemonDetailUseCase.execute(id: id)
+        
+        XCTAssertNotNil(pokemonDetail, "pokemon detail must not be nil")
+        XCTAssertEqual(pokemonDetail?.pokemon.name, name, "check pokemon detailed name")
+        XCTAssertEqual(pokemonDetail?.height, height, "check pokemon detailed height")
+        XCTAssertEqual(pokemonDetail?.weight, weight, "check pokemon detailed weight")
+        XCTAssertEqual(pokemonDetail?.types.first?.type.name, type, "check pokemon detailed type")
     }
 }
