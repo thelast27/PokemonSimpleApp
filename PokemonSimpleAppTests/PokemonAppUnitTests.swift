760846ead6ee5e3db75af35ca7f976f1cd599f2c
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2024-02-20 14:28:23 +0300

    add tests for fetching pokemon list request

--- PokemonSimpleAppTests/PokemonAppUnitTests.swift
+++ PokemonSimpleAppTests/PokemonAppUnitTests.swift
@@ -12,20 +12,42 @@ import SwiftUI
 class PokemonAppUnitTests: XCTestCase {
     
     var cd: CoreDataManager?
+    var getPokemonListUseCase: GetPokemonListUseCase?
     
     @Environment(\.managedObjectContext) var context
     
     override func setUpWithError() throws {
-      try super.setUpWithError()
-      cd = CoreDataManager()
+        try super.setUpWithError()
+        cd = CoreDataManager()
+        getPokemonListUseCase = GetPokemonListUseCase(pokemonRepository: ExploreRepository.shared)
     }
     
     override func tearDownWithError() throws {
         cd = nil
-      try super.tearDownWithError()
+        getPokemonListUseCase = nil
+        try super.tearDownWithError()
     }
     
     func testCoreData() throws {
-        XCTAssertEqual(cd?.container.viewContext.persistentStoreCoordinator?.persistentStores.count, 1)
+        let storeCount = cd?.container.viewContext.persistentStoreCoordinator?.persistentStores.count
+        XCTAssertEqual(storeCount, 1, "check for storing data in Core Data")
+    }
+    
+    func testGetPokemonList() throws {
+        let limit = 20
+        let offset = 0
+        Task {
+            guard let getPokemonList = try await getPokemonListUseCase?.repository.fetchPokemons(context: context, limit: limit, offset: offset) else { return }
+            XCTAssert(!getPokemonList.isEmpty, "pokemon fetch existence check")
+        }
+    }
+    
+    func testPokemonCount() throws {
+        let limit = 20
+        let offset = 0
+        Task {
+            guard let pokemonCount = try await getPokemonListUseCase?.repository.fetchPokemons(context: context, limit: limit, offset: offset) else { return }
+            XCTAssertEqual(pokemonCount.count, limit, "limit pokemon list request excess")
+        }
     }
 }
